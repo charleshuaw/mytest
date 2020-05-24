8e21e900b17dae99977f4f66ff178b8e65a0a9dd
charleshuaw <841629840@qq.com>
2020-05-24 17:22:01 +0800

    Initial commit

---
+++ src/main/java/com/example/mytest/TestController.java
@@ -0,0 +1,15 @@
+package com.example.mytest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("666...");
+        return "haha...";
+    }
+}
+
