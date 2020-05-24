ffbd51fe64c50dc473cbd7b79dea7d8cda0c9340
charleshuaw <841629840@qq.com>
2020-05-24 17:26:47 +0800

    测试git03

--- src/main/java/com/example/mytest/TestController.java
+++ src/main/java/com/example/mytest/TestController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         int s = 0; //step 1
-        int s2 =2;//step 2
+        int s2 = 2;//step 2
+        int s3 = 3;//step 3
         System.out.println("666...");
         return "haha...";
     }
